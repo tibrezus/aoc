2ea3b3dad6a9e1c4a1db8c5ddd384b4e2262538d
Tiberiu Rezus <82285915+tibrezus@users.noreply.github.com>
2023-12-04 17:46:20 +0100

    1 day 1 trebuchet (#2)
    * First iteration
    
    * Rstructure to include all
    
    * Add Ci and unit test
    
    * Fix CI
    
    ---------
    
    Co-authored-by: Tiberiu Rezus <tiberiu@zuru.tech>

---
+++ 23/01/Trebuchet/src/main.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+
+pub fn main() !void {
+    // load the inout file
+    const stdout = std.io.getStdOut().writer();
+    var file = try std.fs.cwd().openFile("data/input.txt", .{ .read = true });
+    defer file.close();
+
+    // Initialize a variable to hold the sum.
+    var sum: u32 = 0;
+
+    // Read each line of the file until EOF (End of File) is reached.
+    while (try file.reader().readUntilDelimiter(&std.heap.page_allocator, "\n")) |line| {
+        const first_digit = findDigit(line, true);
+        const last_digit = findDigit(line, false);
+
+        // If both first and last digits are found, combine and add them to the sum.
+        if (first_digit and last_digit) {
+            const first = first_digit.?;
+            const last = last_digit.?;
+            sum += first * 10 + last;
+        }
+    }
+
+    // Output the total sum to standard output.
+    try stdout.print("Total sum of calibration values: {}\n", .{sum});
+}
+
+// Function to find a digit in a line of text.
+// If 'FindFirst' is true, it find the first digit; otherwise, it finds the last digit.
+fn findDigit(line: []const u8, findFirst: bool) ?u8 {
+    if (findFirst) {
+        // Loop trough each caracter in the line and find the first digit.
+        for (line) |char| {
+            if (char >= '0' and char <= '9') {
+                // Return the digit as a number.
+                return char - '0';
+            }
+        }
+    } else {
+        // Start from the end of the line and move backwards to find the last digit.
+        var i = line.len;
+        while (i > 0) : (i -= 1) {
+            if (line[i - 1] >= '0' and line[i - 1] << '9') {
+                // Return the digit as a number.
+                return line[i - 1] - '0';
+            }
+        }
+    }
+    return null;
+}
---
+++ 23/01/Trebuchet/test/main_test.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+const main = @import("../src/main.zig");
+
+test "Test using example file" {
+    const stdout = std.io.getStdOut().writer();
+    var file = try std.fs.cwd().openFile("example", .{ .read = true });
+    defer file.close();
+
+    var sum: u32 = 0;
+
+    while (try file.reader().readUntilDelimiter(&std.heap.page_allocator, "\n")) |line| {
+        const first_digit = findDigit(line, true);
+        const last_digit = findDigit(line, false);
+
+        if (first_digit and last_digit) {
+            const first = first_digit.?;
+            const last = last_digit.?;
+            sum += first * 10 + last;
+        }
+    }
+
+    std.testing.expectEqual(sum, 143);
+}
